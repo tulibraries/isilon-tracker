8a6fc2b4c816b507332e7fbf2b1f084153ceeca5
Chris Doyle <cdoyle@temple.edu>
2025-05-31 08:44:28 -0400

    add administrate, with dashboards and customized navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :volumes
+      resources :isilon_folders
+      resources :isilon_assets
+
+      root to: "volumes#index"
+    end
+  resources :isilon_assets
+  resources :isilon_folders
+  resources :volumes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
