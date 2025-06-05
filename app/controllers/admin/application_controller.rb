07cd47849ee94217e7ee789fa536c575657fba66
Steven Ng <steven.ng@temple.edu>
2025-06-05 13:57:31 -0700

    Remove administrate's default authentication template

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,12 +6,7 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
     before_action :authenticate_user!
-
-    def authenticate_admin
-      # TODO Add authentication logic here.
-    end
 
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
