2bfccc200631de82c8f4307b94de135c64ab57be
Chris Doyle <cdoyle@temple.edu>
2025-08-04 13:54:35 -0400

    update routes for edit and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,19 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-      resources :isilon_folders
-      resources :isilon_assets
-      resources :volumes
-
+      resources :isilon_folders, except: [:destroy, :edit, :new]
+      resources :isilon_assets, except: [:destroy, :new]
+      resources :volumes, except: [:destroy, :edit, :new]
       root to: "volumes#index"
     end
+
   resources :isilon_assets
   resources :isilon_folders
-
   resources :volumes do
     get :file_tree,          on: :member  # only root folders
     get :file_tree_children, on: :member  # sub-folders + assets
   end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
