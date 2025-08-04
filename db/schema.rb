95c25ff2f1a2b4e934d108bb6fb208f085a1ddad
Steven Ng <steven.ng@temple.edu>
2025-08-04 13:15:04 -0600

    Fix broken schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_01_135810) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_01_214021) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
