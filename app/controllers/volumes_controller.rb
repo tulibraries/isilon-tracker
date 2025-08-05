f3ae2c8ffe08f8335793d5049618723db70f3544
Steven Ng <steven.ng@temple.edu>
2025-08-05 14:47:02 -0600

    Authenticate to access the file tree path

--- app/controllers/volumes_controller.rb
+++ app/controllers/volumes_controller.rb
@@ -1,5 +1,7 @@
 class VolumesController < ApplicationController
   before_action :set_volume, only: %i[ show ]
+  before_action :authenticate_user, only: %i[ show ]
+
     def file_tree
       volume = Volume.find(params[:id])
       root_folders = volume.isilon_folders.where(parent_folder_id: nil)
