4d987260cfa4407fddad91cbcdcdfc3132f1d3ca
Steven Ng <steven.ng@temple.edu>
2025-08-06 11:36:01 -0600

    Revert "IMT-120 Sign in only with oauth2"

--- db/migrate/20250805164449_remove_password_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemovePasswordFromUsers < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :users, :password, :string
-    remove_column :users, :encrypted_password, :string
-    remove_column :users, :reset_password_token
-    remove_column :users, :reset_password_sent_at
-  end
-end
