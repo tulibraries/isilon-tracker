4ccd2385056126a4d49eb3e50d6e9ef1cdc89e86
Chris Doyle <cdoyle@temple.edu>
2025-10-28 13:35:08 -0400

    update schema with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_10_154957) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_12_120000) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_10_154957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "assigned_to"
+    t.text "notes"
     t.index ["assigned_to"], name: "index_isilon_folders_on_assigned_to"
     t.index ["full_path"], name: "index_isilon_folders_on_full_path", unique: true
     t.index ["parent_folder_id"], name: "index_isilon_folders_on_parent_folder_id"
