8dff7d0e4c58c878da14aff2cb1aece33b3fd9ae
Chris Doyle <cdoyle@temple.edu>
2026-02-16 09:30:09 -0500

    add test for backfill script

--- spec/tasks/sync_rake_spec.rb
+++ spec/tasks/sync_rake_spec.rb
@@ -10,7 +10,13 @@ RSpec.describe 'sync rake tasks', type: :task do
   end
 
   before(:each) do
-    Rake::Task['sync:assets'].reenable if Rake::Task.task_defined?('sync:assets')
+    if Rake::Task.task_defined?('sync:assets')
+      Rake::Task['sync:assets'].reenable
+    end
+
+    if Rake::Task.task_defined?('sync:refresh_folder_descendant_assets')
+      Rake::Task['sync:refresh_folder_descendant_assets'].reenable
+    end
   end
 
   describe 'sync:assets' do
@@ -28,6 +34,35 @@ RSpec.describe 'sync rake tasks', type: :task do
 
         Rake::Task['sync:assets'].invoke
       end
+    end
+  end
+
+  describe 'sync:refresh_folder_descendant_assets' do
+    it 'marks ancestors of assets and clears empty folders' do
+      volume = create(:volume, name: 'Test Volume')
+      root_folder = create(:isilon_folder,
+        volume: volume,
+        parent_folder: nil,
+        full_path: '/Root',
+        has_descendant_assets: true)
+      child_folder = create(:isilon_folder,
+        volume: volume,
+        parent_folder: root_folder,
+        full_path: '/Root/Child',
+        has_descendant_assets: false)
+      empty_folder = create(:isilon_folder,
+        volume: volume,
+        parent_folder: nil,
+        full_path: '/Empty',
+        has_descendant_assets: true)
+
+      create(:isilon_asset, parent_folder: child_folder, volume: volume)
+
+      Rake::Task['sync:refresh_folder_descendant_assets'].invoke
+
+      expect(root_folder.reload.has_descendant_assets).to be(true)
+      expect(child_folder.reload.has_descendant_assets).to be(true)
+      expect(empty_folder.reload.has_descendant_assets).to be(false)
     end
   end
 end
