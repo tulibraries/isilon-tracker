d9f2df7a0f718db65fd924fb7ff47db0e2b12383
Chris Doyle <cdoyle@temple.edu>
2026-03-13 09:00:09 -0400

    Move secure tmp log setup to entrypoint

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+<<<<<<< Updated upstream
 rails db:prepare
 rails users:sync_initial
+=======
+mkdir -p /secure-tmp/log
+chmod 700 /secure-tmp/log
+
+rails db:migrate 2>/dev/null || rails db:setup
+>>>>>>> Stashed changes
 rm -f /app/.internal_test_app/tmp/pids/server.pid
 
 exec "$@"
